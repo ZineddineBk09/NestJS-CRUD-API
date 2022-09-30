aa6c21dd6198faa2a92b68c1d21539436dca670a
Zineddine <eino1209@gmail.com>
2022-09-30 18:15:21 +0100

    Auth with JWT Access Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,9 +3,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { Prisma } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+
 @Injectable({})
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     // hash the password
@@ -21,10 +28,10 @@ export class AuthService {
       });
 
       // remove the hash from the user object before returning
-      delete user.hash;
+      //delete user.hash;
 
       // return the user
-      return user;
+      return this.signJwtToken(user.id, user.email);
     } catch (err) {
       if (err instanceof Prisma.PrismaClientKnownRequestError) {
         // P2002 is a unique constraint violation
@@ -58,9 +65,35 @@ export class AuthService {
     }
 
     // remove the hash from the user object before returning
-    delete user.hash;
+    //delete user.hash;
 
     // if password is correct, return the user
-    return user;
+    return this.signJwtToken(user.id, user.email);
+  }
+
+  /**
+   *
+   * @param userId
+   * @param email
+   * @returns
+   */
+
+  async signJwtToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId, // sub is the subject of the token, which is the user id (JWT standard)
+      email,
+    };
+
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: this.config.get('JWT_SECRET'),
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
