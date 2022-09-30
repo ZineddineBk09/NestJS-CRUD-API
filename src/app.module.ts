27364a7b87b387418e7642976f79dbabc25278b3
Zineddine <eino1209@gmail.com>
2022-09-30 17:57:24 +0100

    Just Auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserModule } from './user/user.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     AuthModule,
-    UserModule,
-    BookmarkModule,
     PrismaModule,
   ],
 })
